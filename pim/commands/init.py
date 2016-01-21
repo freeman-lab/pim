8491d92b136958893f8f5fc40cbc622667ebc4ef
freeman <the.freeman.lab@gmail.com>
2016-01-21 01:24:37 -0500

    split up package name from overall name

--- pim/commands/init.py
+++ pim/commands/init.py
@@ -1,18 +1,20 @@
 import os
-import click
 import getpass
-from clint.textui import puts, colored, indent, prompt, validators
-from ..utils import write, retrieve
+import click
+from click import echo
+from clint.textui import indent, puts, prompt
+from ..utils import write, retrieve, success
 
 @click.command('init', short_help='initialize a project', options_metavar='<options>')
 @click.option('--force/--no-force', default=False, help='Force overwrite existing files.')
 def init(force):
     """
-    Initialize a folder with typical contents for a python module.
+    This command initializes a folder with the typical contents of a Python package.
+    After running this and writing your code, you should be ready to publish your package.
     """
-    puts('\nThis utility will help you set up a new python module for publishing on PyPi!\n')
-    puts('After answering a few questions, it will create a few files.')
-    puts('\nPress ^C at any time to bail!\n')
+    echo('\nThis utility will help you set up a new python module for publishing on PyPi!\n')
+    echo('After answering a few questions, it will create a few files.')
+    echo('\nPress ^C at any time to bail!\n')
 
     d = {}
     d['name'] = prompt.query('name:', default=os.getcwd().split('/')[-1])
@@ -25,27 +27,30 @@ def init(force):
     d['repository'] = prompt.query('repository:', default=gitrepo, validators=[])
     d['readme'] = prompt.query('readme:', default='README.md')
     d['license'] = prompt.query('license:', default='MIT')
-    # TODO add a package which is the name underscored
+    d['package'] = prompt.query('package name:', default=d['name'].replace('-','_'))
     d['entry'] = prompt.query('entry point:', default='main.py')
     d['description'] = prompt.query('description:', default='', validators=[])
     
-    puts('\nReady to create the following files:')
+    echo('\nReady to create the following files:')
 
     with indent(4, quote='  -'):
         puts('setup.py')
         puts('setup.cfg')
         puts('MANIFEST.in')
-        puts(d['name'] + '/' + '__init__.py')
-        puts(d['name'] + '/' + d['entry'] + '.py')
+        puts(d['package'] + '/' + '__init__.py')
+        puts(d['package'] + '/' + d['entry'])
         puts('requirements.txt')
 
     finalize = prompt.yn('\nSound like a plan?', default='y')
-    
+
     if finalize:
-        # TODO use force
-        write('requirements.txt')
-        write('setup.py', fields=d)
-        write('setup.cfg', fields=d, stringify=False)
-        write('MANIFEST.in', fields=d, stringify=False)
-        write('__init__.py', fields=d, folder=d['name'])
-        write(d['entry'], folder=d['name'])
+        echo('')
+        write('requirements.txt', force=force)
+        write('setup.py', fields=d, force=force)
+        write('setup.cfg', fields=d, stringify=False, force=force)
+        write('MANIFEST.in', fields=d, stringify=False, force=force)
+        write('__init__.py', fields=d, folder=d['package'], force=force)
+        write(d['entry'], folder=d['package'], force=force)
+        echo('')
+
+    success('Your package is initialized!')
